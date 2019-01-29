1c7278cb604905e781debbd24e49d8efe41e3aba
Andrey Lechev <leechy@leechy.ru>
2019-01-29 14:51:29 +0100

    Set client_id for all http-requests (API) to be set automatically

---
+++ src/app/core/client_id.interceptor.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+/**
+ * Injects the client_id param into each HTTP request.
+ */
+@Injectable()
+export class ClientIdInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    req = req.clone({
+      setParams: {
+        client_id: environment.clientId
+      }
+    });
+    return next.handle(req);
+  }
+}
